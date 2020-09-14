7a1fdce77eb5ac5965c892721817e933789028ae
kiranware13 <kiranware13@gmail.com>
2020-09-14 18:13:46 +0530

    lastName and salary field added setters and getters added

--- application1/src/main/java/com/poc/git/application1/Employee.java
+++ application1/src/main/java/com/poc/git/application1/Employee.java
@@ -3,5 +3,39 @@ package com.poc.git.application1;
 public class Employee {
 	private Integer employeeId;
 	private String employeeFirstName;
+	private String employeeLastName;
+	private Double salary;
+
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeFirstName() {
+		return employeeFirstName;
+	}
+
+	public void setEmployeeFirstName(String employeeFirstName) {
+		this.employeeFirstName = employeeFirstName;
+	}
+
+	public String getEmployeeLastName() {
+		return employeeLastName;
+	}
+
+	public void setEmployeeLastName(String employeeLastName) {
+		this.employeeLastName = employeeLastName;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
 
 }
